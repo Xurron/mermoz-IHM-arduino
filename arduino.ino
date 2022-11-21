90429709a7b3d8f8bbf18c82fd2c25e0adcd255e
Xurron <46415255+Xurron@users.noreply.github.com>
2022-11-21 16:15:33 +0100

    part5
    programme de la partie 5

--- arduino.ino
+++ arduino.ino
@@ -16,37 +16,31 @@ void setup() {
 void loop() {
   while(Serial.available() > 0) {
     incoming = Serial.read();
-    Serial.print("Code ascii envoyé :");
-    Serial.print(incoming);
+    Serial.println("Code ascii envoyé :");
+    Serial.println(incoming);
     Serial.println();
     switch(incoming) {
       case 118:
       FS90.write(0);
-      FS90R.write(180);
-      Serial.print("FS90 : Angle mini");
-      Serial.println();
-      Serial.print("FS90R : Vitesse maxi horaire");
-      Serial.println();
+      write360(1, 100);
+      Serial.println("FS90 : Angle mini");
+      Serial.println("FS90R : Vitesse maxi horaire");
       Serial.println();
       break;
 
       case 98:
       FS90.write(90);
-      FS90R.write(90);
-      Serial.print("FS90 : Angle moyen");
-      Serial.println();
-      Serial.print("FS90R : Arrêt");
-      Serial.println();
+      write360(0, 0);
+      Serial.println("FS90 : Angle moyen");
+      Serial.println("FS90R : Arrêt");
       Serial.println();
       break;
     
       case 110:
       FS90.write(180);
-      FS90R.write(0);
-      Serial.print("FS90 : Angle maxi");
-      Serial.println();
-      Serial.print("FS90R : Vitesse maxi anti-horaire");
-      Serial.println();
+      write360(0, 100);
+      Serial.println("FS90 : Angle maxi");
+      Serial.println("FS90R : Vitesse maxi anti-horaire");
       Serial.println();
       break;
       
@@ -54,5 +48,15 @@ void loop() {
       Serial.println("Erreur de code");
       break;
     }
+  }
+}
+
+int write360(bool x, byte y) {
+  if(x) {
+    float speed1 = 90+(y*0.9);
+    FS90R.write(speed1);
+  } else {
+    float speed0 = 90-(y*0.9);
+    FS90R.write(speed0);
   }
 }
